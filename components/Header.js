6c8f0d1143d5c6ff2f0b9d1eb0a485fb2b360770
femarcano <santrox@gmail.com>
2023-02-06 23:06:43 +0900

    Added style for the Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header>
-            <div>
-                <h1>Next xkcd</h1>
-            </div>
+        <header className="flex justify-between items-center border-2 border-blue-500 p-4 max-w-xl m-auto rounded-md">
+            <h1 className="font-bold">Next<span className="font-light">xkcd</span></h1>
             <nav>
-                <ul>
+                <ul className="flex flex-row gap-2  text-sm font-bold">
                     <li><Link href="/">Home</Link></li>
-                    <li><Link href="/">About</Link></li>
-                    <li><Link href="/">Search</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/search">Search</Link></li>
                 </ul>
             </nav>
+
         </header>
     )
 }

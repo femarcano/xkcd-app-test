8698b49aaa4b1b368eb1a9d0fc8fa23cd1f9a52b
femarcano <santrox@gmail.com>
2023-02-13 22:52:29 +0900

    Refactor layout component

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,7 +1,116 @@
-export default function Comic() {
+import { Layout } from '@/components/Layout'
+import { readdir, readFile, stat } from 'fs/promises'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { basename } from 'path'
+
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
+export default function Comic({
+  id,
+  name,
+  img,
+  alt,
+  title,
+  width,
+  height,
+  hasPrevious,
+  hasNext,
+  prevId,
+  nextId,
+}) {
   return (
     <>
-      <h1>Hola</h1>
+      <Head>
+        <title>xkcd - Comics for developers</title>
+        <meta name="description" content="Comics for developers" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <main>
+          <section className="max-w-lg m-auto">
+            <h1 className="font-bold py-4 text-xl text-center">{title}</h1>
+            <div className="flex justify-center mb-4">
+              <Image
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                  shimmer(width, height)
+                )}`}
+                width={width}
+                height={height}
+                src={img}
+                alt={alt}
+                placeholder="blur"
+              ></Image>
+            </div>
+            <p className="font-medium text-sm">{alt}</p>
+            <div className="flex justify-between mt-4 font-bold">
+              {hasPrevious && <Link href={`/comic/${prevId}`}>⬅️Prev</Link>}
+              {hasNext && <Link href={`/comic/${nextId}`}>Next➡️</Link>}
+            </div>
+          </section>
+        </main>
+      </Layout>
     </>
   )
+}
+
+export async function getStaticPaths() {
+  const files = await readdir('./comics')
+  const paths = files.map((file) => {
+    const id = basename(file, '.json')
+    return { params: { id } }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  console.log(params)
+  const { id } = params
+  const content = await readFile(`./comics/${id}.json`, 'utf-8')
+  const comic = JSON.parse(content)
+
+  const idNumber = +id
+  const prevId = idNumber - 1
+  const nextId = idNumber + 1
+
+  const [prevResult, nextResult] = await Promise.allSettled([
+    stat(`./comics/${prevId}.json`),
+    stat(`./comics/${nextId}.json`),
+  ])
+
+  const hasPrevious = prevResult.status === 'fulfilled'
+  const hasNext = nextResult.status === 'fulfilled'
+
+  return {
+    props: {
+      ...comic,
+      hasPrevious,
+      hasNext,
+      prevId,
+      nextId,
+    },
+  }
 }

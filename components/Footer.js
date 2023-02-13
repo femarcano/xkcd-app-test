0f5b43e5fde4ce772df2081411522c6c54d13772
femarcano <santrox@gmail.com>
2023-02-13 22:51:22 +0900

    Add Footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center">
+      <a href="https://xkcd.com" target="_blank" rel="noopener noreferrer">
+        All comics by xkcd.com
+      </a>
+    </footer>
+  )
+}
